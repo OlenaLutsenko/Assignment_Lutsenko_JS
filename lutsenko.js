f2c2cfa998b75eb5bc8491a0c1833260d246ff80
Olena Lutsenko <113932476+OlenaLutsenko@users.noreply.github.com>
2022-11-17 15:01:50 +0200

    Update lutsenko.js

--- lutsenko.js
+++ lutsenko.js
@@ -9,7 +9,7 @@ greaterSeven(6);
 
 
 //Make an algorithm: if the entered name matches Вячеслав, then output “Привет, Вячеслав”, if not, then output “Нет такого имени”
-function viacheslav(name) {
+function printName(name) {
     if (name === "Вячеслав") {
         console.log("Привет, Вячеслав"); 
     } else {
@@ -17,15 +17,15 @@ function viacheslav(name) {
     };
 };
 
-viacheslav("Вячеслав");
-viacheslav("Вася");
+printName("Вячеслав");
+printName("Вася");
 
 
 //Make an algorithm: there is a numeric array at the input, it is necessary to output the elements of the array that are multiples of 3
 var array = [3, 5, 9, 10, 12];
-var arrayDevidedOn3 = array.filter(function (numbers) {
+var arrayDivisibleOn3 = array.filter(function (numbers) {
     return numbers % 3 == 0;
 });
 
-console.log(arrayDevidedOn3);
+console.log(arrayDivisibleOn3);
 
